6eafbc9ecc1f6ed020239e2552a2fa1490bae059
daskain <daskain@gmail.com>
2018-05-23 03:25:49 +0300

    Проверить доступность по модификаторам

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 package App;
 
-import java.io.PrintStream;
+
 import java.util.*;
 import java.lang.*;
-
-import App.Name;
-import App.Squer;
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hellow, world" + '!');
+        System.out.print("И как же тебя зовут, дорогой друг? ");
+        Scanner namePr = new Scanner(System.in);
+        String res = namePr.next();
+        System.out.println("Hellow, world, " + res + '!');
         System.out.print("Введите длину стороны а = ");
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
--- Squer.java
+++ Squer.java
@@ -1,11 +1,11 @@
 package App;
-import java.util.*;
 
-public class Squer {
 
-    double a, h, p, s;
+class Squer {
 
-    public double squer(double a) {
+    private static double a, h, p, s;;
+
+    static double squer(double a) {
         p = 0.5 * (a * 3);
         h = Math.sqrt((Math.pow(a, 2) - Math.pow(a/2,2)));/* h = (Math.sqrt(p * (3 * (p - a)))) / a; */
         s = 0.5 * a * h;
